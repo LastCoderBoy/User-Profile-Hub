2228984c13eabb92e99668205c9da503a4a5a06a
Jasurbek Khamroev <khamroevapple@gmail.com>
2026-05-09 11:41:20 +0200

    Initial Commit

---
+++ src/main/java/com/jk/User_Profile_Hub/UserProfileHubApplication.java
@@ -0,0 +1,13 @@
+package com.jk.User_Profile_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/User_Profile_Hub/UserProfileHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.User_Profile_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
